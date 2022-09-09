8090b69d05f9cce766f9d44a86c126f742fe8815
Ukemeedem <ukemedem4hire@gmail.com>
2022-09-08 19:17:28 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: always 0
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilinqual puzzle
+* Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+        puts("\"Programming is like building a multilinqual puzzle");
+        return (0);
