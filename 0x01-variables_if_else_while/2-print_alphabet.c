9d3098b5c9fb3b16826c7b313dd0d5aca86d34a2
Ukemeedem <ukemedem4hire@gmail.com>
2022-09-10 18:07:42 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,14 @@
 /**
  * main - entry point
  *
- * Return: always return 0
+ * Return: always returns 0
  *
  */
 int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <== 'z'; a++)
 	{
 		putchar(a);
 	}			
