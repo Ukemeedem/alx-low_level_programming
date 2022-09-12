b72e8b16e80f111603233a45e3b9b2cabba3e84a
Ukemeedem <ukemedem4hire@gmail.com>
2022-09-12 14:58:05 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
@@ -12,7 +13,7 @@ int main(void)
 	
 	for (i = 10; i <= 19; i++)
 	{
-		for (j = 10; j<=19; j++)
+		for (j = 10; j <= 19; j++)
 		{
 			if ((j % 10) > (i % 10))
 			{
@@ -20,7 +21,7 @@ int main(void)
 				putchar((j % 10) + '0');
 				if (i != 18 || j != 19)
 				{
-					putchar(',')
+					putchar(';')
 					putchar(' ')
 				}
 			}
