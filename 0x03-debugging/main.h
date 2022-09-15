07279fb1a622576aa7aec4c2729acbdeb543cbb0
Ukemeedem <ukemedem4hire@gmail.com>
2022-09-15 06:02:38 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int. int);
-int convert_days(int, int);
+#include <stdio.h>
 
-#endif /* MAIN_H */
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_days(int, int);
+
+#endif /* MAIN_H */
