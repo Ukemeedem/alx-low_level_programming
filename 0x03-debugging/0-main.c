45475fec385595c0fe18c55650171bf9a74f4e2a
Ukemeedem <ukemedem4hire@gmail.com>
2022-09-15 04:03:40 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests fuction that prints if the integer is poaitive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_negative(i);
+
+return (0);
+}
